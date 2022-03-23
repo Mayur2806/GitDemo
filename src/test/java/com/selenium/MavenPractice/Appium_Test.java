2e51d7c055bb6299df8718242aa37c262c25f943
Mayur2806 <patilmayur2806@gmail.com>
2022-03-23 12:26:23 +0530

    QAT by Mr X

--- src/test/java/com/selenium/MavenPractice/Appium_Test.java
+++ src/test/java/com/selenium/MavenPractice/Appium_Test.java
@@ -7,6 +7,7 @@ public class Appium_Test {
 	@Test
 	public void Login() {
 		System.out.println("Inside Appium practice");
+		System.out.println("QAT work");
 		System.out.println("commit by mr y");
 		System.out.println("commit by mr x");
 		
