15352c74abe209a523472564b4d501b6815c2086
Mayur2806 <62990160+Mayur2806@users.noreply.github.com>
2022-03-24 12:08:05 +0530

    Update Appium_Test.java

--- src/test/java/com/selenium/MavenPractice/Appium_Test.java
+++ src/test/java/com/selenium/MavenPractice/Appium_Test.java
@@ -12,6 +12,7 @@ public class Appium_Test {
 		System.out.println("commit by mr x");
 		System.out.println("QAT work by mr y");
 		System.out.println("commit from eclipseas");
+		System.out.println("git changes");
 		
 	}
 }
